0e6e3b35fc85ba5e92ef0af85ae8d71348956081
rchamorrocrugeiras <rchamorrocrugeiras@danielcastelao.org>
2026-09-17 13:41:49 +0200

    Añadido procesadores

--- src/Tarea1/InformeSistema.java
+++ src/Tarea1/InformeSistema.java
@@ -2,26 +2,13 @@ package Tarea1;
 
 public class InformeSistema {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+
+        int procesadores = runtime.availableProcessors();
+
         System.out.println("PROCESADORES");
-        System.out.println("====================================================");
-        System.out.println("    Disponibles para JVM: " );
-        System.out.println("    (son hilos lógicos: con SMT no coinciden con los núcleos físicos)\n");
-
-        System.out.println("MEMORIA · ANTES");
-        System.out.println("====================================================");
-        System.out.println("    Total reservada: " + " MiB");
-        System.out.println("    Libre:  " + " MiB");
-        System.out.println("    En uso: " + " MiB");
-        System.out.println("    Máxima (-Xmx): " + " MiB\n");
-
-        System.out.println("MEMORIA · DESPUÉS DE RESERVAR 64 MIB");
-        System.out.println("====================================================");
-        System.out.println("    Total reservada: " + " MiB");
-        System.out.println("    Libre:  " + " MiB");
-        System.out.println("    En uso: " + " MiB");
-        System.out.println("    Máxima (-Xmx): " + " MiB");
-        System.out.println("    Incremento en uso: " + " MiB");
-        System.out.println();
+        System.out.println("=======================");
+        System.out.println("Disponibles JVM: " + procesadores);
     }
 }
