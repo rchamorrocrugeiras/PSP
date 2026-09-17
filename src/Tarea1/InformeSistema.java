4f7225d369412b3e1073293163239e323b2d32b3
rchamorrocrugeiras <rchamorrocrugeiras@danielcastelao.org>
2026-09-17 13:54:18 +0200

    Añadida memoria

--- src/Tarea1/InformeSistema.java
+++ src/Tarea1/InformeSistema.java
@@ -10,5 +10,28 @@ public class InformeSistema {
         System.out.println("PROCESADORES");
         System.out.println("=======================");
         System.out.println("Disponibles JVM: " + procesadores);
+
+        System.out.println();
+        System.out.println("MEMORIA · ANTES");
+        System.out.println("=======================");
+
+        long total = runtime.totalMemory();
+        long libre = runtime.freeMemory();
+        long maximo = runtime.maxMemory();
+
+        long uso = total - libre;
+        long mib = 1024 * 1024;
+
+        long totalMiB = total / mib;
+        long libreMiB = libre / mib;
+        long usoMiB = uso / mib;
+        long maximoMiB = maximo / mib;
+
+        long porcentaje = uso * 100 / total;
+
+        System.out.println("Total reservada: " + totalMiB + " MiB");
+        System.out.println("Libre: " + libreMiB + " MiB");
+        System.out.println("En uso: " + usoMiB + " (" + porcentaje + " % de la total)");
+        System.out.println("Máxima (-Xmx): " + maximoMiB + " MiB");
     }
 }
