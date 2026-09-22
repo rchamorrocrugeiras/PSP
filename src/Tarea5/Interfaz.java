c388c2dcb01f9516394bab5346d72937c519f59d
rchamorrocrugeiras <rchamorrocrugeiras@danielcastelao.org>
2026-09-22 10:11:02 +0200

    Comenzando con el Lanzador

--- src/Tarea5/Interfaz.java
+++ src/Tarea5/Interfaz.java
@@ -1,4 +1,13 @@
 package Tarea5;
 
+import java.util.Scanner;
+
 public class Interfaz {
+
+    public static void main(String[] args){
+
+        Scanner teclado = new Scanner(System.in);
+
+
+    }
 }
