9c64f9afe60c5c22a0448c78844438ba5fe67bc6
rchamorrocrugeiras <rchamorrocrugeiras@danielcastelao.org>
2026-09-22 10:06:27 +0200

    Error corregido, hecho el lanzador

--- src/Tarea5/Interfaz.java
+++ src/Tarea5/Interfaz.java
@@ -1,41 +1,4 @@
 package Tarea5;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class Interfaz {
-
-    public static int ejecutarFactor(String numero) {
-
-        int codigoSalida = -1;
-
-        try {
-            ProcessBuilder pb = new ProcessBuilder("factor", numero);
-            Process proceso = pb.start();
-            BufferedReader lector = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
-
-            String linea;
-
-            while ((linea = lector.readLine()) != null) {
-                System.out.println(linea);
-            }
-
-            BufferedReader errores = new BufferedReader(new InputStreamReader(proceso.getErrorStream()));
-
-            while ((linea = lector.readLine()) != null) {
-                System.out.println(linea);
-            }
-
-            codigoSalida = proceso.waitFor();
-
-            lector.close();
-            errores.close();
-        }
-
-        catch (Exception e) {
-            System.out.println("Error al ejecutar el factor", e.getMessage());
-        }
-
-        return codigoSalida;
-    }
 }
